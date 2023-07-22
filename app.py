49618a2c35aa6973dbb4fd32832b4395a9fa5f0a
Grahulprdxn <rahulg@prdxn.com>
2023-07-22 22:19:29 +0530

    Added Update functionality

--- app.py
+++ app.py
@@ -34,11 +34,20 @@ def products():
     return render_template("show.html")
 
 
-@app.route('/update/<int:sno>')
-def update():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return render_template("show.html")
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
