9c687f84865d5efc09add9ead02143a710ff0945
Grahulprdxn <rahulg@prdxn.com>
2023-07-22 21:49:44 +0530

    Added delete functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,17 +16,36 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    todo = Todo(title="First Todo", desc="Lets get started")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template("index.html")
-    # return 'HEllo Flask!'
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template("index.html", allTodo=allTodo)
 
-@app.route('/products')
+@app.route('/show')
 def products():
-    return render_template("products.html")
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return render_template("show.html")
+
+
+@app.route('/update/<int:sno>')
+def update():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return render_template("show.html")
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
